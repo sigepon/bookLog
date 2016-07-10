45a3edc0492b18c6aaa44389058d4ba22d4c95ea
Kikuchi Shigeo <K@sigepon.local>
2016-07-10 23:18:54 +0900

    一回完成

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,27 @@
+class ReviewsController < ApplicationController
+    def new
+        @book = Book.find(params[:book_id])
+        @review = Review.new
+    end
+
+    def create
+        @book = Book.find(params[:book_id])
+
+        @review = Review.new(
+            book_id: params[:book_id],
+            user_id: current_user.id,
+            body: review_params["body"]
+        )
+        if @review.save
+            redirect_to book_url(@review.book)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:body)
+    end
+end
